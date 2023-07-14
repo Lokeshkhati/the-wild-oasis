eff07c639f25be75e8844a7d31599690b20f5080
lokesh <lokeshkhati005@gmail.com>
2023-07-14 11:47:14 +0530

    add cabins page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,6 +1,16 @@
+import Row from "../ui/Row"
+import Heading from "../ui/Heading"
+import { useEffect } from "react"
+import { getCabins } from "../services/apiCabins"
+
 const Cabins = () => {
+    useEffect(() => {
+        getCabins().then((data) => console.log(data))
+    }, [])
     return (
-        <div>Cabins</div>
+        <Row type='horizontal'>
+            <Heading as='h1'>All Cabins</Heading>
+        </Row>
     )
 }
 export default Cabins
