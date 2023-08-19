dce8b3219146e64c10b2e8c14279a97bcf506e19
lokesh <lokeshkhati005@gmail.com>
2023-08-20 00:55:13 +0530

    crete -service for delete cabina & create cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -13,4 +13,35 @@ const getCabins = async () => {
     return data
 }
 
-export { getCabins }
+
+const createCabin = async (newCabin) => {
+
+    console.log(newCabin, 'api')
+    const { data, error } = await supabase
+        .from('cabins')
+        .insert([newCabin])
+    console.log(data, 'data')
+    if (error) {
+        throw new Error('Cabin could not be created')
+    }
+
+    return data
+
+}
+
+const deleteCabin = async (id) => {
+    console.log(id, 'id')
+    const { data, error } = await supabase
+        .from('cabins')
+        .delete()
+        .eq('id', id)
+    console.log(data, 'delete data')
+    if (error) {
+        throw new Error('Cabin could not be deleted')
+    }
+
+    return data
+
+}
+
+export { getCabins, createCabin, deleteCabin }
