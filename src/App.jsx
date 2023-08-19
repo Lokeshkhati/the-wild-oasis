d45142e1a1e0cccd19fef8dbfa84ba69a3cc2acd
lokesh <lokeshkhati005@gmail.com>
2023-08-20 00:54:08 +0530

    set up toast

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-
+import { Toaster } from 'react-hot-toast'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000
+      // staleTime: 60 * 1000
+      staleTime: 0
     }
   }
 })
@@ -34,51 +35,23 @@ const App = () => {
           <Route path='login' element={<Login />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
+        <Toaster position="top-center" gutter={12} containerStyle={{ margin: '8px' }} toastOptions={{
+          success: {
+            duration: 3000
+          },
+          error: {
+            duration: 500
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)"
+          }
+        }} />
       </QueryClientProvider>
     </>
   )
 }
 export default App
-
-
-// import styled from "styled-components"
-// import GlobalStyles from "./styles/GlobalStyles";
-// import Input from "./ui/Input";
-// import Button from "./ui/Button";
-// import Heading from "./ui/Heading";
-// import Row from "./ui/Row";
-
-// function App() {
-
-//   const StyledApp = styled.main`
-// background-color:white;
-// padding:20px
-// `;
-//   return (
-//     <>
-//       <GlobalStyles />
-//       <StyledApp>
-//         <Row >
-//           <Row type='horizontal'>
-//             <Heading as='h1'>The Wild Oasis</Heading>
-//             <div>
-//               <Heading as='h2'>check in and out</Heading>
-//               <Button>Check in</Button>
-//               <Button>Check out</Button>
-//             </div>
-//           </Row>
-
-//           <Row >
-//             <Heading as='h3'>Form</Heading>
-//             <form>
-//               <Input type='number' aria-label="guest" placeholder="Number of guests" />
-//               <Input type='number' aria-label="guest" placeholder="Number of guests" />
-//             </form>
-//           </Row>
-//         </Row>
-//       </StyledApp>
-//     </>
-//   )
-// }
-
-// export default App
