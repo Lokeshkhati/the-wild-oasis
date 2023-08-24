d8587e4b2980c99799bdacfc852c40d72f37f7b2
lokesh <lokeshkhati005@gmail.com>
2023-08-25 00:59:07 +0530

    refactor

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,19 +1,4 @@
-import styled, { css } from "styled-components"
-
-const Button = styled.button`
-font-size:1.4rem;
-padding: 1.2em 1.6rem;
-font-weight:500;
-border:none;
-border-radius: var(--border-radius-sm);
-background-color:var(--color-brand-600);
-color:var(--color-brand-50);
-cursor:pointer;
-box-shadow:var(--shadow-sm);
-
-&:hover{
-background-color: var(--color-brand-700);}
-`;
+import styled, { css } from "styled-components";
 
 const sizes = {
   small: css`
@@ -39,6 +24,7 @@ const variations = {
   primary: css`
     color: var(--color-brand-50);
     background-color: var(--color-brand-600);
+
     &:hover {
       background-color: var(--color-brand-700);
     }
@@ -47,6 +33,7 @@ const variations = {
     color: var(--color-grey-600);
     background: var(--color-grey-0);
     border: 1px solid var(--color-grey-200);
+
     &:hover {
       background-color: var(--color-grey-50);
     }
@@ -54,10 +41,25 @@ const variations = {
   danger: css`
     color: var(--color-red-100);
     background-color: var(--color-red-700);
+
     &:hover {
       background-color: var(--color-red-800);
     }
   `,
 };
 
-export default Button
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
+`;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
+
+export default Button;
