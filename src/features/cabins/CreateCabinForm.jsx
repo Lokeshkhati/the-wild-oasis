1b9571271f2e5020ac47d82e1788f40c07537331
lokesh <lokeshkhati005@gmail.com>
2023-08-24 22:43:01 +0530

    refactor code

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -8,7 +8,7 @@ import Textarea from "../../ui/Textarea";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { isCreating, createCabin } = useCreateCabin()
   const { isEditing, editCabin } = useEditCabin()
   const isWorking = isCreating || isEditing
@@ -22,10 +22,16 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   const onSubmit = (data) => {
     const image = typeof data.image === 'string' ? data.image : data.image[0]
     if (isEditSession) editCabin({ newCabinData: { ...data, image }, id: editId }, {
-      onSuccess: (data) => reset()
+      onSuccess: (data) => {
+        reset();
+        onCloseModal?.()
+      }
     })
     else createCabin({ ...data, image }, {
-      onSuccess: (data) => reset()
+      onSuccess: (data) => {
+        reset();
+        onCloseModal?.()
+      }
     })
   }
 
@@ -34,7 +40,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)} type={onCloseModal ? 'modal' : 'regular'}>
 
       <FormRow label="Cabin name" error={errors?.name?.message} >
         <Input type="text" id="name" disabled={isWorking} {...register('name', {
@@ -82,7 +88,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
       </FormRow>
 
       <FormRow>
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" onClick={() => onCloseModal?.()}>
           Cancel
         </Button>
         <Button disabled={isWorking}> {isEditSession ? "Edit Cabin" : "Add cabin"}</Button>
