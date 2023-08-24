ea58779b8db9acd54e7639198ab9abd38a816ee9
lokesh <lokeshkhati005@gmail.com>
2023-08-25 00:45:55 +0530

    refactor

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -13,30 +13,7 @@ const AddCabin = () => {
                 <CreateCabinForm />
             </Modal.Window>
 
-            {/* <Modal.Open opens='table'>
-            <Button>Add new cabin</Button>
-        </Modal.Open>
-        <Modal.Window name='table'>
-            <CreateCabinForm />
-        </Modal.Window> */}
         </Modal>)
 }
-
-// const AddCabin = () => {
-//     const [isOpenModal, setIsOpenModal] = useState(false)
-
-//     return (
-//         <div>
-//             <Button onClick={() => setIsOpenModal((show) => !show)}>
-//                 Add new cabin
-//             </Button>
-//             {isOpenModal && (
-//                 <Modal onClose={() => setIsOpenModal(false)}>
-//                     <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//                 </Modal>
-//             )}
-//         </div>
-//     )
-// }
 export default AddCabin
 
