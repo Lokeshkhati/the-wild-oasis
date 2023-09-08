8b03b3e54dccc8a5ffa79a8f444747e61401c70a
lokesh <lokeshkhati005@gmail.com>
2023-09-08 08:40:18 +0530

    implement table filter and sort functionality

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,13 +3,14 @@ import Heading from "../ui/Heading"
 import CabinTable from "../features/cabins/CabinTable"
 import { useState } from 'react'
 import AddCabin from "../features/cabins/AddCabin"
+import CabinTableOperations from "../features/cabins/CabinTableOperations"
 const Cabins = () => {
     const [show, setShow] = useState(false)
 
     return (
         <><Row type='horizontal'>
             <Heading as='h1'>All Cabins</Heading>
-            <p>Filter/Sort</p>
+            <CabinTableOperations />
         </Row>
             <Row>
                 <CabinTable />
