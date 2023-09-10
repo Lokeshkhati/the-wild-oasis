afdc74a0b1d95c6ca64e0a4021b3b6082f7a3814
lokesh <lokeshkhati005@gmail.com>
2023-09-10 06:14:36 +0530

    implement sorting and filtering to bookings

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -1,6 +1,34 @@
+import SortBy from "../../ui/SortBy";
+import Filter from "../../ui/Filter";
+import TableOperations from "../../ui/TableOperations";
 const BookingTableOperations = () => {
+    const filterOptions = [
+        { value: "all", label: "All" },
+        { value: "checked-out", label: "Checked out" },
+        { value: "checked-in", label: "Checked in" },
+        { value: "unconfirmed", label: "Unconfirmed" },
+    ]
+
+    const sortOptions = [
+        { value: "startDate-desc", label: "Sort by date (recent first)" },
+        { value: "startDate-asc", label: "Sort by date (earlier first)" },
+        {
+            value: "totalPrice-desc",
+            label: "Sort by amount (high first)",
+        },
+        { value: "totalPrice-asc", label: "Sort by amount (low first)" },
+    ]
     return (
-        <div>BookingTableOperations</div>
+        <TableOperations>
+            <Filter
+                filterField="status"
+                options={filterOptions}
+            />
+
+            <SortBy
+                options={sortOptions}
+            />
+        </TableOperations>
     )
 }
 export default BookingTableOperations
--- src/services/apiBooking.js
+++ src/services/apiBooking.js
@@ -1,13 +1,25 @@
 import supabase from "./supabase";
 
-export const getBookings = async () => {
-    let { data, error } = await supabase
+export const getBookings = async ({ filter, sortBy }) => {
+
+    let query = supabase
         .from('bookings')
         .select(
             "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
             { count: "exact" }
         );
 
+    // FILTER
+    if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+    // SORT
+    if (sortBy)
+        query = query.order(sortBy.field, {
+            ascending: sortBy.direction === "asc",
+        });
+
+
+    let { data, error } = await query
     if (error) {
         console.log(error)
         throw new Error('Bookings could not be loaded')
