ed9e010340614284a156ec66d2cf04a02937890b
lokesh <lokeshkhati005@gmail.com>
2023-09-10 07:19:29 +0530

    add prefetching

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,9 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { getBookings } from "../../services/apiBooking"
 import { useSearchParams } from "react-router-dom"
 
 export function useBookings() {
+    const queryClient = useQueryClient()
     const [searchParams] = useSearchParams()
 
     // FILTER
@@ -19,10 +20,25 @@ export function useBookings() {
 
     const { isLoading,
         data: { data: bookings, count } = {}, error } = useQuery({
-
             queryKey: ['bookings', filter, sortBy, page],
             queryFn: () => getBookings({ filter, sortBy, page })
         })
+
+    // PREFETCHING
+    const pageCount = Math.ceil(count / PAGE_SIZE)
+
+    if (page < pageCount) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page + 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page + 1 })
+        })
+    }
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page - 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page - 1 })
+        })
+    }
 
     return { isLoading, bookings, error, count }
 }
