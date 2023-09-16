f2015ec3e2ffedbf369761fa6697fabd48bf3724
lokesh <lokeshkhati005@gmail.com>
2023-09-16 16:30:05 +0530

    add Error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorFallback from './ui/ErrorFallback'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary FallBackComponent={ErrorFallback} onReset={() => window.location.replace('/')}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
