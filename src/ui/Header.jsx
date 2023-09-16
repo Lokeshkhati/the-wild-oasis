9bde9710a869c8291f20e38f084f2de5a5f8610a
lokesh <lokeshkhati005@gmail.com>
2023-09-16 08:09:18 +0530

    refactor

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserAvatar from '../features/authentication/UserAvatar'
 import { styled } from "@tanstack/react-query-devtools/build/lib/utils";
 
 const StyledHeader = styled.header`
-background-color:var(--color-grey-0)
+background-color:var(--color-grey-0);
 padding:1.2rem 4.8rem;
 border-bottom: 2px solid var(--color-grey-100);
 display:flex;
